d5257e71000397b998be0b8e0fbdff38cd6869c9
arionrefat <arionrefat@gmail.com>
2022-01-17 20:02:00 +0600

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# JUSTABOT
+
+<h3> Dependency </h3>
+
+1. [Npm](https://nodejs.org/en/download/)
+2. [TickTick](https://ticktick.com/) account
+
+<h3> Usage </h3>
+
+1. Create an account for TickTick using Email only.
+2. Go to root folder and run `npm install`
+3. Create `config.json` file in the root directory. This is where you should put your discord 'token' and ticktick account 'email' and 'password'. The file should look like this:
+```
+{
+	"token": "#########################",
+    "email": "johndoe@gmail.com",
+    "passwords": "abc123"
+}
+```
+4. Then run `node index.js` to run locally
+
+
---
+++ index.js
@@ -0,0 +1,52 @@
+const { Client, Intents } = require('discord.js')
+const { token, email, passwords } = require('./config.json')
+const ticktick = require('ticktick-wrapper')
+
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+})
+
+client.once('ready', () => {
+    console.log('Bot Ready')
+})
+
+client.login(token)
+
+const tickticktask = async () => {
+    await ticktick.login({
+        email: {
+            username: email,
+            password: passwords,
+        },
+    })
+    const tasks = await ticktick.tasks.getUncompleted()
+    titles = tasks.map((t) => t.title)
+    dates = tasks.map((t) => t.dueDate)
+}
+
+tickticktask()
+
+client.on('messageCreate', (msg) => {
+    if (msg.content === '!task') {
+        msg.channel.sendTyping()
+
+        if (titles.length == 0) msg.channel.send("No Tasks for now!")
+        else{
+            for (let i = 0; i < titles.length; i++) {
+                if (dates[i] == null){
+                    msg.channel.send(`${titles[i]}`)
+                }
+                else {
+                    msg.channel.send(`${titles[i]} \t\t ${dates[i].replace(/T.*/g, '')}`)
+                }
+            }
+        }
+        msg.react('❤️')
+    }
+
+    else if (msg.content === '!update'){
+        tickticktask()
+        msg.react('👍')
+        msg.channel.send("Done")
+    }
+})
